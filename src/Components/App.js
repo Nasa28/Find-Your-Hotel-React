9f306bdede9df5ca8e686e4219553bdb04e78709
Nasa28 <kaluchinasaagu@gmail.com>
2021-12-13 18:59:06 +0100

    Create About page

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Footer from './Footer';
 // import Hotel from './Hotel';
 import HotelList from '../Containers/HotelList';
+import HotelDetails from '../Containers/HotelDetails';
+import Error from './Error';
+import About from './About';
 
 function App() {
   return (
